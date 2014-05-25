0d290aeb275785f600bb8d45f774b20ef987a32d
MagaBenG <ben--g2@hotmail.com>
2014-05-24 22:48:23 -0400

    Made Code Neat For Debug

--- TimeTable/src/TimeTable/Main.java
+++ TimeTable/src/TimeTable/Main.java
@@ -1,23 +1,13 @@
 package TimeTable;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
 public class Main {
-
-	public static void main(String args[]){
+	
+	public static void main(String args[]) throws Exception{
 		
-		String WindowName= "Window";
-		
-		JFrame Window = new JFrame(WindowName);
-		
-		Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		Window.setVisible(true);
-		
-		Window.setSize(700, 700);
+			Window.Window(args);
 		
 	}
+	
 	
 }
 
